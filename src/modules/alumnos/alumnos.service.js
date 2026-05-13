93eb5582aa39f879338bd2bcf1b5676f2a902557
DoctourDot18Pup <21031430@itcelaya.edu.mx>
2026-05-12 23:34:31 -0600

    fix(deps): reemplazar bcrypt por bcryptjs para compatibilidad con Vercel serverless

--- src/modules/alumnos/alumnos.service.js
+++ src/modules/alumnos/alumnos.service.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const supabase = require('../../config/supabase');
 const { AppError } = require('../../middlewares/errorHandler.middleware');
 
